882d2c00e1d026f98e476d1ff282427e81e5f3ec
sairoutine <sairoutine@gmail.com>
2015-02-22 01:17:35 +0900

    edit dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 
+if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
+	mkdir -p ~/.vim/bundle
+	git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+fi
