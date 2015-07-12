b66ecbd1f26757ff842f81290d96f67e7c3d0043
sairoutine <sairoutine@gmail.com>
2015-07-12 18:10:44 +0900

    add gitconfig

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,6 +7,7 @@ DIR=$(cd $(dirname $0); pwd)
 
 ln -snf ~/dotfiles/.vimrc ~/.vimrc
 ln -snf ~/dotfiles/.vim ~/.vim
+ln -snf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -snf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
