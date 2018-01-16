546f1cb8c9a5409c34724d6364c3d839539715dc
sairoutine <sairoutine@gmail.com>
2018-01-17 04:07:04 +0900

    add: .railsrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -15,4 +15,5 @@ ln -snf ~/dotfiles/.jshintrc ~/.jshintrc
 ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -snf ~/dotfiles/.ssh/config ~/.ssh/config
 ln -snf ~/dotfiles/.gdbinit ~/.gdbinit
+ln -snf ~/dotfiles/.railsrc ~/.railsrc
 chmod 0600 ~/.ssh/config
